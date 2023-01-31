5b2313ff3acc80046e21f934181d4bdfa814f4f8
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-31 14:48:06 -0800

    pass prime test
    Co-authored-by: David Jandron <jandron1211@gmail.com>
    Asia Kaplanyan <a.kaplanyan@gmail.com>

--- __tests__/euler.test.js
+++ __tests__/euler.test.js
@@ -1,8 +1,24 @@
 import { Multiples } from './../src/euler.js';
+import { Fibonacci } from './../src/euler.js';
+import { Prime } from './../src/euler.js';
 
 describe('Multiples', () => {
 
     test('should return sum of all the multiples of 3 or 5 up to 1000.', () => {
         expect(Multiples()).toBe(234168);
+    });
+});
+
+describe('Fibonacci', () => {
+
+    test('it should find the sum of the even-valued terms in the Fibonacci sequence whose values do not exceed four million.', () => {
+        expect(Fibonacci()).toBe(4613732);
+    });
+});
+
+describe ('Prime', () => {
+
+    test('it should return the largest prime factor of the number 13195', () => {
+        expect(Prime(13195)).toBe(29);
     });
 });
--- src/euler.js
+++ src/euler.js
@@ -11,3 +11,41 @@ export function Multiples() {
     }
     return sum;
 }
+
+export function Fibonacci() {
+    let i = 1;
+    let sum = 0;
+    let fib = [1, 2];
+    while (i < 1001) {
+        fib.push(fib[i] + fib[i - 1]);
+        i++;
+    }
+    fib.forEach(function (element) {
+        if ((element % 2) === 0 && element < 4000000) {
+            sum = sum + element;
+        }
+    });
+    return sum;
+}
+
+const isPrime = num => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+}
+
+export function Prime() {
+  let number = 13195;
+  let index1 = Math.round(number / 2);
+  let primes = [];
+  while (index1 > 0) {
+    if ((number % index1) === 0) {
+      if (isPrime(index1) === true) {
+        primes.push(index1);
+      }
+    }
+    index1--;
+  }
+  return primes[0];
+}
